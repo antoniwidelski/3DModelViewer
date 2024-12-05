2a9f89e161ffbe3a4b47ac5b16b4c2a25f6208fb
Antoni Widelski <antoniwidelski@gmail.com>
2024-12-05 20:38:38 +0100

    Renderer clean-up

--- 3DModelViewer/src/Graphics/Renderer.cpp
+++ 3DModelViewer/src/Graphics/Renderer.cpp
@@ -17,20 +17,6 @@ void Renderer::Clear() const
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-/*void Renderer::Draw(const Model& model, const Shader& shader) const
-{
-	std::cout << "Size: " << model.m_Meshes.size() << std::endl;
-	for (int i = 0; i < model.m_Meshes.size(); i++)
-		Draw(model.m_Meshes[i], shader);
-}
-
-void Renderer::Draw(const Mesh& mesh, const Shader& shader) const
-{
-	mesh.m_Texture.Bind();
-	Draw(mesh.m_VA, mesh.m_IB, shader);
-	std::cout << "Drawuje mesha" << std::endl;
-}*/
-
 void Renderer::SetTexture(const Texture& tex, const Shader& shader)
 {
 	//shader.SetUniformInt("texture_diffuse1", 0);
